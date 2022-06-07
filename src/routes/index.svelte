9cebbc10c2e990cd7dd8d336b363da90e182b8c7
Jacob Sides <sides798@gmail.com>
2022-06-07 14:37:58 -0400

    Basic styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,64 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+
+
+
+<article>
+    <h1>Jacob Sides</h1>
+    <h2>Full-stack Developer</h2>
+    <p>
+        I'm a passionate JavaScript developer, diving deep into front-end and
+        headless technologies, hoping to build interactive 3D sites in the
+        future. If I'm not trying out a new JS framework, you'll find me gaming,
+        jamming to music or attempting to pick up a new skill, whether it's the
+        Bass or German.
+    </p>
+
+    <h3 class="mb-0 -ml-0.5">My Work</h3>
+    <p class="text-sm mt-1 pt-0">
+        I've been fortunate enough to work with fantastic designers at Pyxl to
+        build out their visions.
+    </p>
+    <p>
+        <a href="https://brownlee.co" target="_blank" rel="nofollow">
+            Brownlee
+        </a>
+        - A Next.js Shopify Plus Headless Ecommerce
+    </p>
+    <p>
+        <a href="https://trimech.com" target="_blank" rel="nofollow">
+            Trimech
+        </a>
+        - WordPress Woocommerce site with Vue.js search and archive functionalities
+    </p>
+    <p>
+        <a href="https://mlilyusa.com" target="_blank" rel="nofollow">
+            MLILY
+        </a>
+        - WordPress Woocommerce site built with TailwindCSS, Twig and Vue.js archives
+    </p>
+    <p>
+        <a href="https://talkiatry.com" target="_blank" rel="nofollow">
+            Talkiatry
+        </a>
+        - WordPress site built with TailwindCSS, Twig and Vue.js archives
+    </p>
+    <h3>Side Projects</h3>
+    All my repos are primarily side projects that focus more concepts, teaching and
+    learning tech-stacks rather than front-end development. Nonetheless, see how
+    obsessed I am with the Spotify API.
+</article>
+<style lang="css">
+    article {
+        font-family: 'Roboto', sans-serif;
+        padding: 1em;
+        max-width: 1000px;
+        margin: auto;
+    }
+    h1 {
+        font-size: 2em;
+    }
+
+</style>
